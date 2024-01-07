a721d80ef9d13317faa6a8bc8b7aa6331edd3ff6
Habeeb Salami <1849920+habeeb-salami@users.noreply.github.com>
2024-01-07 17:32:09 +0100

    Extractor Class and Main class was rewritten

--- Extractor.java
+++ Extractor.java
@@ -9,9 +9,11 @@ import java.util.List;
 public class Extractor {
 
     public static void main(String[] args) {
-        PhoneNumberFilter numberFilter = new PhoneNumberFilter();
-        String[] sampleNumbers = { "2351234567", "08067654321", "1234567890", "0812345678", "2359876543" };
-        List filteredNumbers = numberFilter.filterNumbers(sampleNumbers);
+        PhoneNumberFilter numberFilter = new PhoneNumberFilter("^(0807|0812|0806|234806|\\+234806)\\d{8}$");
+        String[] sampleNumbers = { "2348061234567", "08067654321", "1234567890", "0812345678", "2359876543",
+                "+23480687654321" }; // Added numbers for testing
+        List<String> filteredNumbers = numberFilter.filterNumbers(sampleNumbers);
+
         System.out.println("Valid phone numbers:");
         for (String number : filteredNumbers) {
             System.out.println(number);
--- PhoneNumberFilter.java
+++ PhoneNumberFilter.java
@@ -3,18 +3,39 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PhoneNumberFilter {
-    public List filterNumbers(String[] phoneNumbers) {
-        List validNumbers = new ArrayList<>();
-        String pattern = "^(235|0806)\\d{8}$"; // Regex pattern for matching desired format
-        Pattern compiledPattern = Pattern.compile(pattern);
+    private String numberPatterns;
+
+    public PhoneNumberFilter() {
+    }
+
+    public PhoneNumberFilter(String numberPatterns) {
+        this.numberPatterns = numberPatterns;
+    }
+
+    public List<String> filterNumbers(String[] phoneNumbers, String numberPatterns) {
+        return this.filterEngine(phoneNumbers, numberPatterns);
+    }
+
+    public List<String> filterNumbers(String[] phoneNumbers) {
+        return this.filterEngine(phoneNumbers, this.numberPatterns);
+    }
+
+    private List<String> filterEngine(String[] phoneNumbers, String patterns) {
+        List<String> validNumbers = new ArrayList<>();
+        Pattern compiledPattern = Pattern.compile(patterns);
         for (String phoneNumber : phoneNumbers) {
             Matcher matcher = compiledPattern.matcher(phoneNumber);
             if (matcher.matches()) {
                 validNumbers.add(phoneNumber);
             }
         }
+
         return validNumbers;
     }
 }
