45502142096995bc903740037851d22be461aaec
Habeeb Salami <1849920+habeeb-salami@users.noreply.github.com>
2024-01-09 11:31:39 +0100

    email extractor  tested on the main method

--- EmailFilter.java
+++ EmailFilter.java
@@ -5,18 +5,26 @@ import java.util.regex.Pattern;
 
 public class EmailFilter {
 
-    public List<String> filterEmails(String text) {
-        List<String> emails = new ArrayList<>();
-        Pattern emailPattern = Pattern.compile("[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+"); // Robust email pattern
-        Matcher matcher = emailPattern.matcher(text);
+    public EmailFilter(String email) {
 
+    }
+
+    public EmailFilter() {
+    }
+
+    public List<String> filterEmails(String text) {
+        return this.filterEngine(text);
+    }
+
+    private List<String> filterEngine(String rawText) {
+        List<String> emails = new ArrayList<>();
+        Pattern emailPattern = Pattern.compile("[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+"); // Robust email pattern                                                                                                 
+        Matcher matcher = emailPattern.matcher(rawText);
         while (matcher.find()) {
             String email = matcher.group();
             emails.add(email);
         }
-
         return emails;
     }
 
-   
 }
--- Extractor.java
+++ Extractor.java
@@ -9,10 +9,11 @@ import java.util.List;
 public class Extractor {
 
     public static void main(String[] args) {
-
+        phoneNumbersExtractor();
+        emailExtractor();
     }
 
-    private void phoneNumbersExtractor() {
+    private static void phoneNumbersExtractor() {
         PhoneNumberFilter numberFilter = new PhoneNumberFilter("^(0807|0812|0806|234806|\\+234806)\\d{8}$");
         String[] sampleNumbers = { "2348061234567", "08067654321", "1234567890", "0812345678", "2359876543",
                 "+23480687654321" }; // Added numbers for testing
@@ -24,7 +25,7 @@ public class Extractor {
         }
     }
 
-    private void emailExtractor() {
+    private static void emailExtractor() {
         EmailFilter filter = new EmailFilter();
         String sampleText = "Contact us at support@example.com or sales@example.net for more information.";
         List<String> filteredEmails = filter.filterEmails(sampleText);
